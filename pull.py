929e4a8532d0ccd215ee42d6d487bb3ee7355eae
noyuno <noyuno@users.noreply.github.com>
2018-04-24 13:53:07 +0900

    Update

--- pull.py
+++ pull.py
@@ -1,45 +1,53 @@
 from mastodon import Mastodon
 import os
+import sys
 
-url="https://s.noyuno.space"
-def create():
-    Mastodon.create_app(
-        'pleromabot',
-        api_base_url = url,
-        to_file = 'pytooter_clientcred.secret'
-    )
+url=""
+email=""
+password=""
+secret=""
 
-def main():
+def create(filename):
+    global url
+    global email
+    global password
+    global secret
+    with open(filename,"r") as f:
+        r=f.readlines()
+        secret=r[0].replace("\n", "") + ".secret"
+        url="https://"+r[0].replace("\n","")
+        email=r[1].replace("\n","")
+        password=r[2].replace("\n","")
+    if os.path.exists(secret) == False:
+        Mastodon.create_app(
+            'pleromabot',
+            api_base_url = url,
+            to_file = secret
+        )
+
+def toot():
+    global url
+    global email
+    global password
     mastodon = Mastodon(
-        client_id = 'pytooter_clientcred.secret',
+        client_id = secret, 
         api_base_url = url
     )
-    email=""
-    password=""
-    with open("user.secret","r") as f:
-        r=f.readlines()
-        email=r[0].replace("\n","")
-        password=r[1].replace("\n","")
-
-    print(email)
-    print(password)
 
     mastodon.log_in(
         email,
         password,
-        to_file = 'pytooter_usercred.secret'
+        to_file = 'user.secret'
     )
 
-    # Create actual API instance
     mastodon = Mastodon(
-        client_id = 'pytooter_clientcred.secret',
-        access_token = 'pytooter_usercred.secret',
+        client_id = secret,
+        access_token = 'user.secret',
         api_base_url = url
     )
     mastodon.toot('ちゃろー')
 
 if __name__ == '__main__':
-    if os.path.exists("pytooter_clientcred.secret") == False:
-        create()
-    main()
+    create(sys.argv[1])
+    toot()
 
